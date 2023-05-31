eda00fba2948237731ba0eb04339ccfcf2bf7b02
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-06-01 08:49:17 +0900

    제네릭, enum 문제

---
+++ class_1_make_simple_game/bin/practice/practice_8_generic_enum.dart
@@ -0,0 +1,58 @@
+enum KeyType { padlock, button, dial, finger }
+
+class StrongBox<E> {
+  E? _data;
+  KeyType keyType;
+
+  StrongBox(this.keyType);
+
+  void put(E data) {
+    _data = data;
+  }
+
+  E? get(int count) {
+    switch (this.keyType) {
+      case KeyType.padlock:
+        if (count == 1025) {
+          return _data;
+        }
+
+        break;
+      case KeyType.button:
+        if (count == 10001) {
+          return _data;
+        }
+
+        break;
+      case KeyType.dial:
+        if (count == 30001) {
+          return _data;
+        }
+
+        break;
+      case KeyType.finger:
+        if (count == 1000001) {
+          return _data;
+        }
+
+        break;
+    }
+    return null;
+  }
+}
+
+void main() {
+  int count = 0;
+  KeyType keyType = KeyType.finger;
+
+  final StrongBox strongBox = StrongBox(keyType);
+  strongBox.put('Congrats');
+
+  for (int i = 0; i < 1000050; i++) {
+    final data = strongBox.get(count);
+    if (data != null) {
+      print('$keyType $i : $data');
+    }
+    count++;
+  }
+}
