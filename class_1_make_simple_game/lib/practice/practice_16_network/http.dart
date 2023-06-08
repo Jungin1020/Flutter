b5d84093a03e8d47060d93b511f803e35f744cca
Jungin1020 <97022661+Jungin1020@users.noreply.github.com>
2023-06-08 14:17:49 +0900

    일반화

--- class_1_make_simple_game/lib/practice/practice_16_network/http.dart
+++ class_1_make_simple_game/lib/practice/practice_16_network/http.dart
@@ -4,12 +4,12 @@ import 'package:http/http.dart' as http;
 // as http 를 안쓰면 http.get 이 아니라 .get 탑 레벨 메서드들만 쓰게 되기 떄문에
 
 void main() async {
-  print(await getTodo());
+  print(await getTodo(1));
 }
 
-Future<Map<String, dynamic>> getTodo() async {
-  final http.Response response =
-      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+Future<Map<String, dynamic>> getTodo(int id) async {
+  final http.Response response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/$id'));
   //응답에 대한 정보를 가지고 있음
   return jsonDecode(response.body);
 
